dnbert/prm

commit 10b48a5420710bc661b733f4127816964eb6659d

Test if Release.gpg exists before trying to recreate
Move Release.tmp into Release, minimizing race condition

prm.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 	s.name        = 'prm'
-	s.version     = '0.0.5'
-	s.date        = '2013-01-21'
+	s.version     = '0.0.6'
+	s.date        = '2013-02-20'
 	s.summary     = "Package Repository Manager"
 	s.description = "PRM (Package Repository Manager) is an Operating System independent Package Repository tool. PRM supports Repository syncing to DreamObjects"
 	s.authors     = ["Brett Gailey"]

prm.rb
@@ -3,7 +3,7 @@
 require 'prm/trollop'
 require 'prm/repo'
 
-version_info = "0.0.5"
+version_info = "0.0.6"
 
 opts = Trollop::options do
     version "Package Repository Manager #{version_info} - 2012 Brett Gailey"

prm/repo.rb
@@ -121,15 +121,19 @@ module Debian
             file.read
         }).result(binding)
 
-        release_file = File.new("#{path}/dists/#{release}/Release","wb")
+        release_file = File.new("#{path}/dists/#{release}/Release.tmp","wb")
         release_file.puts erb
         release_file.close
+
+        FileUtils.move("#{path}/dists/#{release}/Release.tmp", "#{path}/dists/#{release}/Release")
     end
 
     # We expect that GPG is installed and a key has already been made
     def generate_release_gpg(path,release)
         Dir.chdir("#{path}/dists/#{release}") do
-            system "gpg --yes --output Release.gpg -ba Release"
+            unless File.exist?("Release.gpg")
+                system "gpg --yes --output Release.gpg -ba Release"
+            end
         end
     end
 end

commit 02ec06fd79f43313a44be4a4b153640cfedab197

Hotfix for gpg related errors caused by: 10b48a5420710bc661b733f4127816964eb6659d

prm.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
 	s.name        = 'prm'
-	s.version     = '0.0.7'
+	s.version     = '0.0.8'
 	s.date        = '2013-02-20'
 	s.summary     = "Package Repository Manager"
 	s.description = "PRM (Package Repository Manager) is an Operating System independent Package Repository tool. PRM supports Repository syncing to DreamObjects"

prm.rb
@@ -3,7 +3,7 @@
 require 'prm/trollop'
 require 'prm/repo'
 
-version_info = "0.0.7"
+version_info = "0.0.8"
 
 opts = Trollop::options do
     version "Package Repository Manager #{version_info} - 2012 Brett Gailey"

prm/repo.rb
@@ -131,9 +131,7 @@ module Debian
     # We expect that GPG is installed and a key has already been made
     def generate_release_gpg(path,release)
         Dir.chdir("#{path}/dists/#{release}") do
-            unless File.exist?("Release.gpg")
-                system "gpg --yes --output Release.gpg -ba Release"
-            end
+            system "gpg --yes --output Release.gpg -b Release"
         end
     end
 end