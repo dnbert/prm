94006bdb4828e150614d3469f293be7a420725a0
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2013-12-12 07:54:22 -0800

    Make sure there's one-and-only-one trailing newline.
    This works for me to fix https://github.com/dnbert/prm/issues/33.

--- lib/prm/repo.rb
+++ lib/prm/repo.rb
@@ -130,7 +130,7 @@ module Debian
 
             write_mutex.synchronize do
                 # Copy the control file data into the Packages list
-                d.write(File.read("tmp/#{tdeb}/control").chomp)
+                d.write(File.read("tmp/#{tdeb}/control").gsub!(/\n+/, "\n"))
                 d.write(package_info.join("\n"))
                 d.write("\n") # blank line between package info in the Packages file
             end
